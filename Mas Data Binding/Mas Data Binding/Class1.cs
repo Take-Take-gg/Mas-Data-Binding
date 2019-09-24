297c85504002896627645c5b11d97d59661b2083
Alumno <Alumno@PC22>
2019-09-24 09:42:27 -0500

    mas cosas con data binding 2.0

---
+++ Mas Data Binding/Mas Data Binding/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mas_Data_Binding
+{
+    class Empleado
+    {
+        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+
+        public static Empleado GetEmpleado()
+        {
+            var emp = new Empleado()
+            {
+                Nombre = "El Take Take", Titulo = "hola que hace"
+            };
+
+            return emp;
+        }
+            
+    }
+}
